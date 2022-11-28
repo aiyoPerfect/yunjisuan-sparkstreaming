9d23bf5fa3c58ff9a2f515f69ef4482de4011fe9
aiyoPerfect <119048180+aiyoPerfect@users.noreply.github.com>
2022-11-28 17:08:07 +0800

    Add files via upload

---
+++ cloudcompute-class/src/main/java/sparkstreaming/sorteddata/descenddata.scala
@@ -0,0 +1,62 @@
+package sparkstreaming.sorteddata
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+
+object descenddata {
+  def main(args: Array[String]): Unit = {
+    //TODO创建环境对象
+
+    val sparkConf = new SparkConf().setMaster("spark://master:7077").setAppName("SparkStreaming")
+
+    val ssc = new StreamingContext(sparkConf, Seconds(3))
+
+
+
+    //kafka
+    val kafkaPara: Map[String, Object] = Map[String, Object](
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "worker1:9092",
+      ConsumerConfig.GROUP_ID_CONFIG -> "cloudcompute",
+      "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
+      "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer"
+    )
+
+    //读取kafka数据创建DStream
+    val kafkaDataDS: InputDStream[ConsumerRecord[String, String]] = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Subscribe[String, String](Set("wordcount"), kafkaPara)
+    )
+    //kafkaDataDS.print()
+    //将每条消息的KV取出
+    val valueDStream: DStream[String] = kafkaDataDS.map(record => record.value())
+
+    //计算wordcount
+    val wordadd = valueDStream.flatMap(_.split(" "))
+      .map((_, 1))
+      .reduceByKey(_+_)
+    wordadd.print()
+
+
+    //DStrea的transform方法表示对底层的RDD进行操作并返回结果
+    val sortedResultDS: Unit = wordadd.foreachRDD(rdd => {
+      val sortRDD: RDD[(String, Int)] = rdd.sortBy(t=>t._2,ascending = false)
+      val top10: Array[(String, Int)] = sortRDD.take(10)
+      print("=======top10=======\n")
+      top10.foreach(println)
+      print("=======top10=======\n")
+    })
+
+
+
+
+    //开启任务
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
